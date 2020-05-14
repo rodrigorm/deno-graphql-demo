9f1a961003ae29555dcf7135ad1f55f0549620b8
Rodrigo Moyle <rodrigorm@gmail.com>
2020-05-14 13:23:34 +0000

    TODO

---
+++ api/graphql/index.ts
@@ -0,0 +1,10 @@
+// Error: 502 Bad Gateway
+// import { ApolloServer, gql } from 'https://cdn.pika.dev/apollo-server-lambda@2.13.1';
+// Error: relative import path "crypto" not prefixed with / or ./ or ../
+// import { ApolloServer, gql } from 'https://dev.jspm.io/apollo-server-lambda@2.13.1';
+// import * as pkg from 'https://cdn.pika.dev/sha.js';
+
+// import * as pkg from 'https://unpkg.com/apollo-server-lambda@2.13.1/dist/index.js';
+import * as pkg from 'https://dev.jspm.io/apollo-server-lambda@2.13.1';
+// import * as pkg from 'https://cdn.pika.dev/apollo-server-lambda@2.13.1';
+console.log(pkg);
